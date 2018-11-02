0b16f68772df0ca733aa575fe144a31cf85340a9
Stephen Walmsley <stewalmsley@Stephens-MacBook-Pro.local>
2018-11-02 19:01:57 +0000

    using smaller jpg file and importing it

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import dogImg from '../assets/dog.jpg';
 
 class Avatar extends Component {
     state = {
         imgError: false
     }
     render() {
+        console.log(dogImg)
         const { avatar_url, height, width } = this.props
-        return <div><img src={avatar_url}alt="Avatar" onError={this.state.imgError ? null : this.switchURL} height={height} width={width}></img></div> 
+        return <div><img src={avatar_url} alt="Avatar" onError={this.state.imgError ? null : this.switchURL} height={height} width={width}></img></div> 
     }
     switchURL = (event) => {
-        event.target.src= "/dog.jpg"
+        event.target.src=dogImg
         this.setState({
             imgError: true
         })
