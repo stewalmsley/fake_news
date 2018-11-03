c8744ab94e0b73f72530cca236f46051b73f4654
Stephen Walmsley <stewalmsley@Stephens-MacBook-Pro.local>
2018-11-03 06:25:26 +0000

    change behaviour of avatar as was not invoking onError when changed user

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,23 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import dogImg from '../assets/dog.jpg';
 
-class Avatar extends Component {
-    state = {
-        imgError: false
+const Avatar = ({ avatar_url, height, width}) => {
+        return <div><img src={avatar_url} alt="Avatar" onError={switchURL} height={height} width={width}></img></div> 
     }
-    render() {
-        console.log(dogImg)
-        const { avatar_url, height, width } = this.props
-        return <div><img src={avatar_url} alt="Avatar" onError={this.state.imgError ? null : this.switchURL} height={height} width={width}></img></div> 
+    const switchURL = (event) => {
+        const src = event.target.src
+        event.target.src = src === dogImg ? null : dogImg;
     }
-    switchURL = (event) => {
-        event.target.src=dogImg
-        this.setState({
-            imgError: true
-        })
-    }
-};
+
     Avatar.propTypes = {
        avatar_url: PropTypes.string.isRequired, 
        height: PropTypes.string.isRequired, 
