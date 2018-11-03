0fb2d15dae592cae6c40b9aa747b8ad40595df6f
Stephen Walmsley <stewalmsley@Stephens-MacBook-Pro.local>
2018-11-03 19:12:20 +0000

    added user error handling to login screen

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -5,7 +5,8 @@ class LogIn extends Component {
     state = {
         logInUser: '',
         switchUser: '', 
-        password: ''
+        password: '', 
+        incorrectUserName: false
     }
     render() {
         const { users } = this.props
@@ -13,15 +14,18 @@ class LogIn extends Component {
             <div>
                 <h6>Enter username and password </h6>
                 <form name="logInUser" onSubmit={this.handleSubmit}>
-                    <input onChange={this.handleChange} type="text" name="logInUser" value={this.state.logInUser}
+                    <input required onChange={this.handleChange} type="text" name="logInUser" value={this.state.logInUser}
                     placeholder="username - eg jessjelly" size="40"/>
                     <input type="password" onChange={this.handleChange} name="password" value={this.state.password} size="40"
                     placeholder="password will be ignored"/>
                     <input type="submit"/>
                 </form>
+                <div className="loginError">
+                {this.state.incorrectUserName && <span>Incorrect User Name</span>}
+                </div>
                 <h6>Or Select User: </h6>
                 <form name="switchUser" onSubmit={this.handleSubmit}>
-                <select name="switchUser" onChange={this.handleChange}>
+                <select required name="switchUser" onChange={this.handleChange}>
                     <option value="">Select User</option>
                     {users.map(user => {
                         return <option name={user.name} value={user.username} key={user._id}>{user.name}</option>
@@ -41,11 +45,15 @@ class LogIn extends Component {
     handleSubmit = (event) => {
         event.preventDefault()
         const user = this.props.users.find(user => user.username === this.state[event.target.name])
-        this.props.setUser(user)
-        this.setState({
-            logInUser: '', 
-            password : ''
-        })
+        if (!user) this.setState({incorrectUserName: true})
+        else {
+            this.props.setUser(user)
+            this.setState({
+                logInUser: '', 
+                password : '', 
+                incorrectUserName: false
+            })
+        }
     }
 
 }
